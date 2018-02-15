e1db20caaeb8bf3b45c594ea5322f488c57e5ae4
fawazmez <fawaz.mezher@ocsbstudent.ca>
2018-02-15 13:10:18 -0500

    ball and background and name
    added the ball my name iin the text  and the event listener

--- main.lua
+++ main.lua
@@ -2,6 +2,29 @@
 --
 -- main.lua
 --
+-- Created by : Fawaz Mezher
+-- Created on : Feb 2018
+--
+--how to add an event listener, so that when you touch an image you get feedback
 -----------------------------------------------------------------------------------------
 
--- Your code here
+-- show the text fawaz mezher
+ local myText = display.newText( "Fawaz Mezher", 768, 200, native.systemFont, 100 )
+-- text color
+myText:setFillColor( 1, 0, 1 )
+-- makes background white
+display.setDefault( "background", 36, 28, 1 )
+-- displaying the image on the screen
+local soccerBall = display.newImageRect( "./assets/sprites/ball.png", 500, 480 )
+soccerBall.x = display.contentCenterX
+soccerBall.y = display.contentCenterY
+soccerBall.id = "ball object"
+ 
+local function onObjectTouch( event )
+    --the function for touching the ball
+    print( "Touched ball ")
+
+    return true
+end
+-- event listener for touching the ball
+soccerBall:addEventListener( "touch", onObjectTouch )
